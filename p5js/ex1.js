6779a01935ae6e8230262475f07054edd6486d40
André Rocha <steam228@gmail.com>
2019-11-06 17:18:48 +0000

    Initial commit

---
+++ p5js/ex1.js
@@ -0,0 +1,17 @@
+
+
+function setup() {
+  var canvasDiv = document.getElementById('ex1');
+  var width = canvasDiv.offsetWidth;
+  var sketchCanvas = createCanvas(width,width*.75);
+  console.log(sketchCanvas);
+  sketchCanvas.parent('ex1');
+
+
+}
+
+function draw() {
+  background(220);
+
+
+}
